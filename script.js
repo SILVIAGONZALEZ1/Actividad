394975a7744b8e545ac4c0bcb1653955effd92a0
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-12-03 09:08:37 -0300

    se agregan archivos nuevos , flexbox, grid, javascript

---
+++ script.js
@@ -0,0 +1,67 @@
+/*Ejercicios:
+1.Crea una variable nombre con tu nombre y úsala para imprimir un mensaje.*/
+let nombre = `Silvia`;
+console.log(`Hola mi nombre es ${nombre}`);
+
+/*
+2.Declara y suma dos números, muestra el resultado en consola.*/
+let resultado = 10 + 5; 
+console.log(`El resultado es : ${resultado}`);
+
+/*
+3.Usa operadores lógicos para evaluar si 10 > 5 y 5 < 2.
+*/
+let esMayor = 10 > 5; 
+console.log(`El resultado de comparar 10 y 5 me da que, 10 es ${esMayor}`);
+
+let esMenor = 5 < 2;
+console.log(`Es menor ${esMenor}`);
+
+/*
+4.Declara variables con let y const y nota las diferencias al intentar cambiarlas.
+*/
+
+let edad = 15; 
+let edad_2 = 20;
+
+edad = 33;
+edad_2 =88;
+
+console.log(`Mi edad es ${edad_2}`);
+
+
+
+
+/*
+5.Escribe un comentario que explique qué hace tu código.
+Declara una variable de tipo boolean y úsala en una operación lógica.*/
+/*
+Imprime el tipo de dato de una variable usando typeof.
+Declara un array y muestra uno de sus elementos en la consola.
+Usa una operación con % para encontrar el resto de 17 ÷ 3.
+Declara una variable sin valor e imprime su valor inicial.*/
+
+ 
+
+
+
+
+
+
+
+
+
+
+/*let nombre = 'Juan';
+const edad = 12;
+console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años`);
+
+
+/*let suma = 1000.50 / 50000000;
+console.log(`La suma de 10000 + 500000 es ${suma}`);
+
+if (edad >= 18) {
+    console.log(`Eres mayor de edad`);
+}else {
+    console.log(`Eres menor de edad.`);
+}*/
